72a0a040f3e308e2dbfb8c2e233d36cf630759e5
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-01-30 13:27:29 +0100

    Første del done

--- basics.js
+++ basics.js
@@ -1,12 +1,36 @@
+window.onload = function() {
+    document.getElementById('input').value = '';
+    }
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 const number = getRandomInt(100);
 const numberText = document.querySelector(".rnumber");
+const display = document.querySelector("#display");
 
-numberText.textContent = number;
+
 
 
 
 console.log(number);
+function checkNumber(n){
+    if (n === number) {
+        return n + " er det rigtige tal!";
+    } else if(n < number){
+        return "Gæt højere"
+    } else if(n > number){
+        return "Gæt lavere"
+    }
+}
+
+function parse(){
+    let guess = parseInt(document.querySelector("#input").value);
+    console.log(guess);
+    console.log(checkNumber(guess));
+    display.textContent = checkNumber(guess);
+
+}
+
+
+
   
