973846c6749c691b52811706180d5086dc455811
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-01-30 12:10:02 +0100

    opgave 1-2 done

--- basics.js
+++ basics.js
@@ -1,1 +1,12 @@
-console.log("test")
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+const number = getRandomInt(100);
+const numberText = document.querySelector(".rnumber");
+
+numberText.textContent = number;
+
+
+
+console.log(number);
+  
