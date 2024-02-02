2a2c3e1dc9ed7ea414540b4dfc39fe9ad3fd37d6
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-02-02 12:59:44 +0100

    Arealopgave

---
+++ areal/script.js
@@ -0,0 +1,7 @@
+function convertSquareMToSquareFt(n){
+    return n * 10.763910;
+}
+//Unit tests
+console.log(convertSquareMToSquareFt(100));
+console.log(convertSquareMToSquareFt(200));
+console.log(convertSquareMToSquareFt(1500));
---
+++ areal/scriptareal.js
@@ -0,0 +1,7 @@
+function udregnRektangelAreal(h, w){
+    return h * w;
+}
+//Unit tests
+console.log(udregnRektangelAreal(2,5));
+console.log(udregnRektangelAreal(6,5));
+console.log(udregnRektangelAreal(15,1200));
