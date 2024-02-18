ce9ccf7d1efb33b0a746c22d754c1fb5c9cc8161
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-02-18 15:20:21 +0100

    Ved det ikke

--- rioapitest/script.js
+++ rioapitest/script.js
@@ -25,11 +25,16 @@ function fetchData() {
 function displayData(data){
     const name = document.querySelector("#name");
     const rank = document.querySelector("#rank");
+    const rating = document.querySelector("#rating");
     const score = Math.round(data.mythic_plus_scores_by_season[0].segments.all.score);
+    
     name.innerHTML = data.name;
     rank.innerHTML = score;
     //console.log(data.mythic_plus_scores_by_season[0].segments);
     rank.style.color = data.mythic_plus_scores_by_season[0].segments.all.color;
+    let output = `<p><span id="name">${name}</span> har <span id="rank"> ${rank}</span> Rating</p>`;
+
+    rating.outerHTML = output;
 }
 
 window.addEventListener("keydown", (e) =>{
